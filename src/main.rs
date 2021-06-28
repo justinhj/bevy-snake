7d5bcb4b23f94651dad48fd00029380607d5c2c4
Justin Heyes-Jones <justinhj@gmail.com>
2021-06-27 18:15:25 -0700

    snake movement updated

--- src/main.rs
+++ src/main.rs
@@ -59,6 +59,7 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.insert_resource(Materials {
         head_material: materials.add(Color::rgb(0.7,0.7,0.7).into()),
+        segment_material: materials.add(Color::rgb(0.3,0.3,0.3).into()),
         food_material: materials.add(Color::rgb(1.,0.,1.).into()),
     });
 }
@@ -67,10 +68,16 @@ struct SnakeHead {
     direction: Direction,
 }
 
+struct SnakeSegment;
+
+#[derive(Default)]
+struct SnakeSegments(Vec<Entity>);
+
 struct Food;
 
 struct Materials {
     head_material: Handle<ColorMaterial>,
+    segment_material: Handle<ColorMaterial>,
     food_material: Handle<ColorMaterial>,
 }
 
@@ -91,16 +98,25 @@ fn food_spawner(
         .insert(Size::square(0.8));
 }
 
-fn spawn_snake(mut commands: Commands, materials: Res<Materials>) {
-    commands
-        .spawn_bundle(SpriteBundle {
-            material: materials.head_material.clone(),
-            sprite: Sprite::new(Vec2::new(10.0,10.0)),
-            ..Default::default()
-        })
-        .insert(SnakeHead{direction: Direction::Up})
-        .insert(Position { x: 3, y: 3 })
-        .insert(Size::square(0.8));
+fn spawn_snake(mut commands: Commands, materials: Res<Materials>, mut segments: ResMut<SnakeSegments>) {
+    segments.0 = vec![
+        commands
+            .spawn_bundle(SpriteBundle {
+                material: materials.head_material.clone(),
+                sprite: Sprite::new(Vec2::new(10.0,10.0)),
+                ..Default::default()
+            })
+            .insert(SnakeHead{direction: Direction::Up})
+            .insert(SnakeSegment)
+            .insert(Position { x: 3, y: 3 })
+            .insert(Size::square(0.8))
+            .id(),
+            spawn_segment(
+                commands,
+                &materials.segment_material,
+                Position { x: 3, y: 2 },
+                ),
+    ];
 }
 
 fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Sprite)>) {
@@ -148,8 +164,17 @@ fn snake_movement_input(keyboard_input: Res<Input<KeyCode>>, mut heads: Query<&m
     }
 }
 
-fn snake_movement(mut heads: Query<(&mut Position, &SnakeHead)>) {
-    if let Some((mut head_pos, head)) = heads.iter_mut().next() {
+fn snake_movement(segments: ResMut<SnakeSegments>, 
+                  mut heads: Query<(Entity, &SnakeHead)>,
+                  mut positions: Query<&mut Position>, 
+                  ) {
+    if let Some((head_entity, head)) = heads.iter_mut().next() {
+        let segment_positions = segments.0
+            .iter()
+            .map(|e| *positions.get_mut(*e).unwrap())
+            .collect::<Vec<Position>>();
+
+        let mut head_pos = positions.get_mut(head_entity).unwrap();
         match &head.direction {
             Direction::Left => {
                 head_pos.x -= 1;
@@ -164,7 +189,25 @@ fn snake_movement(mut heads: Query<(&mut Position, &SnakeHead)>) {
                 head_pos.y -= 1;
             }
         };
+        segment_positions
+            .iter()
+            .zip(segments.0.iter().skip(1))
+            .for_each(|(pos,segment)| {
+                *positions.get_mut(*segment).unwrap() = *pos;
+            });
     }
+}
+
+fn spawn_segment(mut commands: Commands, material: &Handle<ColorMaterial>, position: Position) -> Entity {
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: material.clone(),
+            ..Default::default()
+        })
+        .insert(SnakeSegment)
+        .insert(position)
+        .insert(Size::square(0.65))
+        .id()
 }
 
 fn main() {
@@ -176,6 +219,7 @@ fn main() {
             ..Default::default()
         })
         .insert_resource(ClearColor(Color::rgb(0.04,0.04,0.04)))
+        .insert_resource(SnakeSegments::default())
         .add_startup_system(setup.system())
         .add_startup_stage("game_setup", SystemStage::single(spawn_snake.system()))
         .add_system(
